19d50815b7b76a7b9e9f635a52a2d9e97d680cbd
LVBK <levanbaokhanh.bkhn@gmail.com>
2019-02-21 18:48:51 +0700

    custom code

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ export default class ReactPdfJs extends Component {
     ]).isRequired,
     page: PropTypes.number,
     onDocumentComplete: PropTypes.func,
+    onSizeMeasureComplete: PropTypes.func,
     scale: PropTypes.number,
     className: PropTypes.string,
   }
@@ -21,6 +22,7 @@ export default class ReactPdfJs extends Component {
     page: 1,
     onDocumentComplete: null,
     scale: 1,
+    onSizeMeasureComplete: null,
   }
 
   state = {
@@ -28,19 +30,7 @@ export default class ReactPdfJs extends Component {
   };
 
   componentDidMount() {
-    const {
-      file,
-      onDocumentComplete,
-      page,
-    } = this.props;
-    PdfJsLib.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.943/pdf.worker.js';
-    PdfJsLib.getDocument(file).then((pdf) => {
-      this.setState({ pdf });
-      if (onDocumentComplete) {
-        onDocumentComplete(pdf._pdfInfo.numPages); // eslint-disable-line
-      }
-      pdf.getPage(page).then(p => this.drawPDF(p));
-    });
+    this.getDocument();
   }
 
   componentWillReceiveProps(newProps) {
@@ -54,6 +44,37 @@ export default class ReactPdfJs extends Component {
     }
   }
 
+  componentDidUpdate(prevProps) {
+    const file = this.props.file;
+    if (prevProps.file !== file) {
+      this.getDocument();
+    }
+  }
+
+  getDocument = () => {
+    const {
+      file,
+      onDocumentComplete,
+      page,
+      onSizeMeasureComplete,
+    } = this.props;
+    PdfJsLib.GlobalWorkerOptions.workerSrc = '//cdnjs.cloudflare.com/ajax/libs/pdf.js/2.0.943/pdf.worker.js';
+    PdfJsLib.getDocument(file).then((pdf) => {
+      this.setState({ pdf });
+      if (onDocumentComplete) {
+        onDocumentComplete(pdf._pdfInfo.numPages); // eslint-disable-line
+      }
+      pdf.getPage(page).then(p => {
+        const fileSize = this.drawPDF(p);
+        return fileSize;
+      }).then((fileSize) => {
+        if (onSizeMeasureComplete) {
+          onSizeMeasureComplete(fileSize); // eslint-disable-line
+        }
+      });
+    });
+  }
+
   drawPDF = (page) => {
     const { scale } = this.props;
     const viewport = page.getViewport(scale);
@@ -61,11 +82,16 @@ export default class ReactPdfJs extends Component {
     const canvasContext = canvas.getContext('2d');
     canvas.height = viewport.height;
     canvas.width = viewport.width;
+    const fileSize = {
+      height: viewport.height / scale,
+      width: viewport.width / scale,
+    } 
     const renderContext = {
       canvasContext,
       viewport,
     };
     page.render(renderContext);
+    return fileSize;
   }
 
   render() {
